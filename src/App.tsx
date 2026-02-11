d596f575d22ced5d0dbc8a0ffa98b792f67c2eaa
eriiiiiiapen <91891293+eriiiiiiapen@users.noreply.github.com>
2026-02-12 00:00:49 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div style={{
+        height: "80vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "4rem"
+      }}>
+        🎥 VTuber Mood Player 🎥
+      </div>
+    </>
+  )
+}
+
+export default App
